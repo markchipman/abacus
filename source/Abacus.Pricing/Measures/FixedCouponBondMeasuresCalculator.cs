c33f5cfbc529e811ade12f746230ce4d6133f0dc
Chris <ckarcz@gmail.com>
2019-01-06 02:29:36 -0500

    start of single measure calc

--- source/Abacus.Pricing/Measures/FixedCouponBondMeasuresCalculator.cs
+++ source/Abacus.Pricing/Measures/FixedCouponBondMeasuresCalculator.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using Abacus.Pricing.Models;
 
 namespace Abacus.Pricing.Measures
 {
-
     public class FixedCouponBondMeasuresCalculator : IInstrumentMeasuresCalculator<FixedCouponBond>
     {
+        private readonly IDictionary<Measure, IInstrumentMeasureCalculator<FixedCouponBond>> measureCalculators = new Dictionary<Measure, IInstrumentMeasureCalculator<FixedCouponBond>>();
+
         public object CalculateMeasures(DateTime valuationDate, FixedCouponBond instrument, params Measure[] measures)
         {
             throw new NotImplementedException();
         }
+    }
+
+    public interface IInstrumentMeasureCalculator<TInstrument>
+    {
+        object CalculateMeasure(DateTime valuationDate, TInstrument instrument);
     }
 }
