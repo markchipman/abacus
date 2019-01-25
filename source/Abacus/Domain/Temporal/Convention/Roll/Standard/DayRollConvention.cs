79a17ce65889bc2ac9d75f345ef5a16502690813
Christopher Karcz <christopher.karcz@caissallc.com>
2019-01-25 13:58:35 -0500

    DayRollConvention - roll forward

--- source/Abacus/Domain/Temporal/Convention/Roll/Standard/DayRollConvention.cs
+++ source/Abacus/Domain/Temporal/Convention/Roll/Standard/DayRollConvention.cs
@@ -22,12 +22,26 @@ namespace Abacus.Domain
             return result;
         }
 
-        public override DateTime RollBackward(DateTime date)
+        public override DateTime RollForward(DateTime date)
         {
-            throw new NotImplementedException();
+            if (date.Day < dayOfMonth)
+            {
+                var daysToAdd = dayOfMonth - date.Day;
+                var result = date.AddDays(daysToAdd);
+                return result;
+            }
+            else
+            {
+                // TODO - checks for 28+ dayOfMonth values
+                var firstOfMonth = new DateTime(date.Year, date.Month, 1);
+                var firstOfNextMonth = firstOfMonth.AddMonths(1);
+                var daysToAdd = dayOfMonth - firstOfNextMonth.Day;
+                var result = date.AddDays(daysToAdd);
+                return result;
+            }
         }
 
-        public override DateTime RollForward(DateTime date)
+        public override DateTime RollBackward(DateTime date)
         {
             throw new NotImplementedException();
         }
